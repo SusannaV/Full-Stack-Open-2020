49606008465b0806aace5d22fcc495a021f3346c
SusannaV <susanna.veijalainen@gmail.com>
2020-07-11 11:59:30 +0300

    siistitty anekdootit

--- Osa 1/anekdootit/src/index.js
+++ Osa 1/anekdootit/src/index.js
@@ -16,63 +16,46 @@ import ReactDOM from 'react-dom'
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
 
- 
+  const ReturnMostVoted = (props) => {
+    let mostVoted = 0;
+    let index = -1;
+    for (let i = 0; i<props.list.length; i++){
+      if (props.list[i]>mostVoted){
+        mostVoted=props.list[i];
+        index = i;
+      }
+    }
+    if (index===-1){
+    return (
+      <> </>
+    )
+    }
+    return(
+      <>
+      <h2>Anecdote with most votes</h2>
+      <p>{props.anecdotes[index]}</p>
+      <p>This anecdote has {props.list[index]} votes</p>
+      </>
+    )
+  }
+
 
 const App = (props) => {
   let points = new Array(anecdotes.length).fill(0)
   
   const [selected, setSelected] = useState(0)
   const [point, setPoint] = useState(points)
-  console.log('Appin sisäinen point', point)
   
   const SelectRandom = () =>{
     let randomNumber = Math.floor((Math.random() * anecdotes.length));
-    console.log(randomNumber)
     setSelected(randomNumber)
   }
 
   let Vote = () => {
-
-    console.log('alkup', point)
-
     let voted = [...point]
-    console.log('voted ennen ', voted)
-    console.log('selected ennen', voted[selected])
     voted[selected] += 1
-    console.log('selected jälkeen', voted[selected])
-    console.log('voted', voted)
     setPoint (voted)
-    console.log('point', point)
   }
-
-  const ReturnMostVoted = () => {
-    let mostVoted = 0;
-    let index = -1;
-    for (let i = 0; i<point.length; i++){
-      if (point[i]>mostVoted){
-        mostVoted=point[i];
-        index = i;
-      }
-    }
-
-    if (index===-1){
-        
-    return (
-      <>
-      </>
-    )
-    }
-
-    return(
-      <>
-      <h2>Anecdote with most votes</h2>
-      <p>{anecdotes[index]}</p>
-      <p>This anecdote has {point[index]} votes</p>
-      </>
-    )
-  }
-
-  
 
     return (
     <div>
@@ -81,19 +64,12 @@ const App = (props) => {
       <p>This anecdote has {point[selected]} votes</p>
       <Button onClick={SelectRandom} text='Give me a new one'/>
       <Button onClick={Vote} text='Vote'/>
-      
-      <ReturnMostVoted/>
-      
-
+      <ReturnMostVoted list={point} anecdotes={anecdotes}/>
     </div>
-    
   )
-  
 }
 
 ReactDOM.render(
-  
   <App anecdotes={anecdotes}/>,
   document.getElementById('root'),
-  
 )
