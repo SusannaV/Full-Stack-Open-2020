950232e3cf12314bb37d45b7f6217c4d945d0881
SusannaV <susanna.veijalainen@gmail.com>
2020-07-13 15:20:53 +0300

    2.4 tehty

--- Osa 2/kurssitiedot2/src/index.js
+++ Osa 2/kurssitiedot2/src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Course = (props) => {
-  console.log("Coursen propsit", props);
-
   const Header = ({ name }) => {
-    return <h1>{name}</h1>;
+    return <h2>{name}</h2>;
   };
 
   const Content = ({ courselist }) => {
-    console.log("Contentin propsit", { courselist });
-
+    const total = courselist.reduce((s, t) => s + t.exercises, 0);
     return (
       <div>
         {courselist.map((part) => (
@@ -33,88 +30,70 @@ const Course = (props) => {
     );
   };
 
-  const total = props.course.parts.reduce((s, t) => s + t.exercises, 0);
-
   return (
     <div>
-      <Header name={props.course.name} />
-      <Content courselist={props.course.parts} />
+      {props.course.map((acourse) => (
+        <div key={acourse.id}>
+          <Header name={acourse.name} />
+          <Content courselist={acourse.parts} />
+        </div>
+      ))}
     </div>
   );
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Testikurssi",
-        exercises: 5,
-        id: 4,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      <h1> Web development curriculum</h1>
+      <Course course={courses} />
     </div>
   );
 };
-
-/*const Header = (props) =>{
-    console.log(props)
-    return(
-      <h1>{props.course}</h1>
-    )
-  }
-  
-  const Part =(props)=>{
-    console.log(props)
-    return(
-      <p>{props.name} {props.exercises}</p>
-    )
-  }
-  
-  const Content = (props) =>{
-    return (
-      <>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
-      </>
-    )
-  }
-  
-  const Total = (props)=>{
-    console.log(props)
-    return (
-      <p>Number of exercises {props.taulukko[0].exercises + props.taulukko[1].exercises + props.taulukko[2].exercises}</p>
-    )
-  }
-  return (
-    <div>
-    <Header course={course.name}/>
-    <Content parts={course.parts} />
-    <Total taulukko={course.parts} />
-    </div>
-  )
-}*/
 
 ReactDOM.render(<App />, document.getElementById("root"));
