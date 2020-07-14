ac1403a7acd19ce6e856fda8485476ed187a2d17
SusannaV <susanna.veijalainen@gmail.com>
2020-07-14 12:00:07 +0300

    2.5 refaktoroitu

--- Osa 2/kurssitiedot2/src/components/Course.js
+++ Osa 2/kurssitiedot2/src/components/Course.js
@@ -1,34 +1,6 @@
 import React from "react";
 
 const Course = (props) => {
-  const Header = ({ name }) => {
-    return <h2>{name}</h2>;
-  };
-
-  const Content = ({ courselist }) => {
-    const total = courselist.reduce((s, t) => s + t.exercises, 0);
-    return (
-      <div>
-        {courselist.map((part) => (
-          <p key={part.id}>
-            <Part oneCourse={part} />
-          </p>
-        ))}
-        <p>
-          <b>Total of {total} excercises</b>
-        </p>
-      </div>
-    );
-  };
-
-  const Part = ({ oneCourse }) => {
-    return (
-      <>
-        {oneCourse.name} {oneCourse.exercises}
-      </>
-    );
-  };
-
   return (
     <div>
       {props.course.map((acourse) => (
@@ -38,6 +10,34 @@ const Course = (props) => {
         </div>
       ))}
     </div>
+  );
+};
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Content = ({ courselist }) => {
+  const total = courselist.reduce((s, t) => s + t.exercises, 0);
+  return (
+    <div>
+      {courselist.map((part) => (
+        <p key={part.id}>
+          <Part oneCourse={part} />
+        </p>
+      ))}
+      <p>
+        <b>Total of {total} excercises</b>
+      </p>
+    </div>
+  );
+};
+
+const Part = ({ oneCourse }) => {
+  return (
+    <>
+      {oneCourse.name} {oneCourse.exercises}
+    </>
   );
 };
 
