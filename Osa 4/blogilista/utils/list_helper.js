2b44d9dca51707d536ac1d3cc9fbc6758cd704c7
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2020-12-13 10:30:16 +0200

    4.4 tehty

--- Osa 4/blogilista/utils/list_helper.js
+++ Osa 4/blogilista/utils/list_helper.js
@@ -1,7 +1,30 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+
+    if (blogs.length===0)
+        return 0
+    if (blogs.length === 1)
+        return blogs[0].likes
+    
+    var likes = blogs.reduce((sum, post) => sum + post.likes,0)
+        return likes
+}
+
+    // const reducer = (sum, item) => {
+    //   return sum + item
+    // }
+  
+    //     blogs.reduce(reducer, 0)
+
+
+
+
+
+
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
