8f9c66886c47a306aeaafc0cc9cbfd866b0df13a
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2023-01-04 11:56:03 +0200

    4.5 tehty

--- Osa 4/blogilista2023/utils/list_helper.js
+++ Osa 4/blogilista2023/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((s, t) => s + t.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  //Funktio selvittää millä blogilla on eniten tykkäyksiä. Jos suosikkeja on monta, riittää että funktio palauttaa niistä jonkun.
+  return blogs.reduce((max, b) => b.likes > max.likes ? b : max, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
