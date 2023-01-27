4dbbb7bac3cc48be0443c68caee7929b15ffbade
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2023-01-27 18:31:56 +0200

    4.20 tehty

--- Osa 4/blogilista2023/utils/middleware.js
+++ Osa 4/blogilista2023/utils/middleware.js
@@ -19,19 +19,29 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  } else if (error.name ===  'JsonWebTokenError') {
+  } else if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: 'token missing or invalid' })
   } else if (error.name === 'TokenExpiredError') {
     return response.status(401).json({
-      error: 'token expired'
+      error: 'token expired',
     })
   }
 
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('bearer ')) {
+    console.log('token:' , authorization.replace('bearer ', ''))
+    request.token=authorization.replace('bearer ', '')
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor,
 }
