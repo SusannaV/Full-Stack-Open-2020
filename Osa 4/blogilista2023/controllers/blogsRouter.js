6b0e655100410c7ff419f6960ceff45a4953d8b6
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2023-01-27 18:31:56 +0200

    4.20 tehty

--- Osa 4/blogilista2023/controllers/blogsRouter.js
+++ Osa 4/blogilista2023/controllers/blogsRouter.js
@@ -8,17 +8,17 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('bearer ')) {
-    return authorization.replace('bearer ', '')
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.startsWith('bearer ')) {
+//     return authorization.replace('bearer ', '')
+//   }
+//   return null
+// }
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
