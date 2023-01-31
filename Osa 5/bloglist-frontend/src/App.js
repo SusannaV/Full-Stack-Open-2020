74a9b226da4d5829669585081cff56e7ebf624b9
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2023-01-31 17:44:40 +0200

    5.1 tehty

---
+++ Osa 5/bloglist-frontend/src/App.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+      // setErrorMessage('wrong credentials')
+      // setTimeout(() => {
+      //   setErrorMessage(null)
+      // }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form> 
+  )
+  
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+       {loginForm()}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ Osa 5/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,7 @@
+const Blog = ({blog}) => (
+  <div>
+    {blog.title} by {blog.author}
+  </div>  
+)
+
+export default Blog
